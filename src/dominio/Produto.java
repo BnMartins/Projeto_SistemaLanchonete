821d133affc9bcd84141220ef5cbcb45842f4690
bmnco <bmnco@users.noreply.github.com>
2026-06-11 20:30:14 -0300

    feat: add CategoriaProduto enum and Produto domain model

---
+++ src/dominio/Produto.java
@@ -0,0 +1,44 @@
+package dominio;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private CategoriaProduto categoria;
+
+    public Produto(int id, String nome, double preco, CategoriaProduto categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: "+id + " | Nome: " + nome + " | Preço: " + preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
